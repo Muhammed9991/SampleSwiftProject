b555a7ac4bdb5a914a23d80f51d856132eb37f0a
Muhammed Mahmood <muhammedmahmood999@gmail.com>
2024-07-04 09:25:47 +0100

    Initial Commit

---
+++ SampleProject/SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectApp.swift
+//  SampleProject
+//
+//  Created by Muhammed Mahmood on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
